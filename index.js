179564bf3a5984d840e734f6e4e8ac73d42dd2f3
sourish1 <sourishkumarmusib@gmail.com>
2018-09-11 19:39:10 +0530

    api set up

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 var express = require('express');
+var firebase = require('firebase');
+var bodyParser = require('body-parser');
+
+//var seedData = require('./users.json');
+
 var app = express();
 
 var port = 3000;
-console.log("hello");
+
+var config = {
+    apiKey: "AIzaSyB-D30973UOGMHxkHxsIsz4QqXmMr3ikQI",
+    authDomain: "user-project-ec48c.firebaseapp.com",
+    databaseURL: "https://user-project-ec48c.firebaseio.com",
+    projectId: "user-project-ec48c",
+    storageBucket: "user-project-ec48c.appspot.com",
+    messagingSenderId: "812920421843"
+  };
+firebase.initializeApp(config);
+
+var ref = firebase.app().database().ref();
+var usersInfoRef = ref.child('usersInformation');
+var usersRef = ref.child('users');
+
+app.use(bodyParser.json());
+
+app.post('/add', (req, res) => {
+    var user = usersRef.push({
+                name: req.body.name,
+                email: req.body.email
+            });
+    var userInfo = usersInfoRef.push({
+                name: req.body.name,
+                email: req.body.email
+            });
+    if(user)
+    {
+        res.status(200).send(user);
+    }
+});
+
+//array-format
+// app.get('/fetch', (req, res) => {
+//     usersRef.on('value', snap =>  {
+//         var names = [];
+//         snap.forEach(name => {
+//            names.push(name.child('name').val());
+//         });
+//         res.status(200).send(names);
+//      });
+// });
+
+app.get('/fetch', (req, res) => {
+    usersRef.on('value', snap =>  {
+        res.status(200).send(snap.val());
+    });      
+});
+
+
 
 app.listen(port);
